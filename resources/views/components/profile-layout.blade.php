bcc30b87839946d3a403aac8070137530b3ff281
unknown <wardahnabilah33@gmail.com>
2023-04-28 20:44:48 +0700

    Create profile-layout

---
+++ resources/views/components/profile-layout.blade.php
@@ -0,0 +1,33 @@
+<x-layout>
+    <main>
+        <div class="container profile">
+            <div class="profile__detail">
+                <img src="{{$profileData['userAvatar']}}" alt="" class=" photo photo--medium profile__detail-image">
+                <div class="profile__detail-text">
+                    <h3 class="name">{{$profileData['username']}}</h3>
+                    @if(auth()->user()->username === $profileData['username'])
+                        <a href="/manage-avatar" class="button button--blue">Change Avatar</a>
+                    @elseif($profileData['alreadyFollowed'])
+                        <form action="/profile/{{$profileData['username']}}/unfollow" method="POST">
+                            @csrf
+                            <button class="button button--red">Unfollow</button>
+                    </form>
+                    @else
+                        <form action="/profile/{{$profileData['username']}}/follow" method="POST">
+                            @csrf
+                            <button class="button button--yellow">+ Follow</button>
+                        </form>
+                    @endif
+                </div>
+            </div>
+            <div class="profile__menu">
+                <a href="/profile/{{$profileData['username']}}" class="{{Request::segment(3) == '' ? 'active' : '' }}">{{$profileData['postCount']}} Posts</a>
+                <a href="/profile/{{$profileData['username']}}/followers" class="{{Request::segment(3) == 'followers' ? 'active' : ''}}">{{$profileData['followersCount']}} Followers</a>
+                <a href="/profile/{{$profileData['username']}}/following" class="{{Request::segment(3) == 'following' ? 'active' : ''}}">{{$profileData['followingCount']}} Following</a>
+            </div>
+            
+            {{$slot}}
+
+        </div>
+    </main>
+</x-layout>
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,48 +1,20 @@
-<x-layout>
-    <main>
-        <div class="container profile">
-            <div class="profile__detail">
-                <img src="{{$userAvatar}}" alt="" class=" photo photo--medium profile__detail-image">
-                <div class="profile__detail-text">
-                    <h3 class="name">{{$username}}</h3>
-                    @if(auth()->user()->username === $username)
-                        <a href="/manage-avatar" class="button button--blue">Change Avatar</a>
-                    @elseif($alreadyFollowed)
-                        <form action="/profile/{{$username}}/unfollow" method="POST">
-                            @csrf
-                            <button class="button button--red">Unfollow</button>
-                        </form>
-                    @else
-                        <form action="/profile/{{$username}}/follow" method="POST">
-                            @csrf
-                            <button class="button button--yellow">+ Follow</button>
-                        </form>
-                    @endif
+<x-profile-layout :profileData="$profileData">
+    <!-- Post Cards -->
+    @foreach($posts as $post)
+        <a href="/post/{{$post->id}}">
+            <div class=" card card__timeline">
+                <div class="card-header-container">
+                    <div class="profile-detail">
+                        <img class="profile-detail__image photo photo--small" src="{{$post->author->avatar}}" alt="">
+                        <span class="profile-detail__name">{{$post->author->username}}</span>
+                    </div>
+                    <div class="small-text posted-on">posted on {{$post->created_at->format('d F Y')}}</div>
+                </div>
+                <div class="card-body-container">
+                    <h6 class="post-title">{{$post->title}}</h6>
+                    <p class="post-body">{{$post->body}}</p>
                 </div>
             </div>
-            <div class="profile__menu">
-                <a href="" class="posts">{{$posts->count()}} Posts</a>
-                <a href="" class="followers">Followers</a>
-                <a href="" class="following">Following</a>
-            </div>
-            <!-- Post Cards -->
-            @foreach($posts as $post)
-                <a href="/post/{{$post->id}}">
-                    <div class=" card card__timeline">
-                        <div class="card-header-container">
-                            <div class="profile-detail">
-                                <img class="profile-detail__image photo photo--small" src="{{$post->author->avatar}}" alt="">
-                                <span class="profile-detail__name">{{$post->author->username}}</span>
-                            </div>
-                            <div class="small-text posted-on">posted on {{$post->created_at->format('d F Y')}}</div>
-                        </div>
-                        <div class="card-body-container">
-                            <h6 class="post-title">{{$post->title}}</h6>
-                            <p class="post-body">{{$post->body}}</p>
-                        </div>
-                    </div>
-                </a>
-            @endforeach
-        </div>
-    </main>
-</x-layout>
+        </a>
+    @endforeach
+</x-profile-layout>
